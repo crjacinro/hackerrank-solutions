ed9d49fdff00ba9e6c4a8689dc9c23149c053ea8
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2020-12-21 21:38:20 +0800

    Setup directory

---
+++ Problem Solving/mini-max-sum/MiniMaxSum.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void exitApp()
+{
+    cout << "\n\nPress Enter to Exit...";
+    cin.get();
+}
+
+int main()
+{
+    //runTestCases();
+    exitApp();
+}
---
+++ Problem Solving/mini-max-sum/build.sh
@@ -0,0 +1,2 @@
+g++ -std=c++11 -O2 -Wall MiniMaxSum.cpp -o MiniMaxSum
+start MiniMaxSum.exe
