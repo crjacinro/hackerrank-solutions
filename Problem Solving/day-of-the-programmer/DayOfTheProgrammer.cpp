420220db14eb403b65d93d47adb84866aadeff12
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2021-01-13 12:36:30 +0800

    Refactor using functions

--- Problem Solving/day-of-the-programmer/DayOfTheProgrammer.cpp
+++ Problem Solving/day-of-the-programmer/DayOfTheProgrammer.cpp
@@ -2,12 +2,61 @@
 
 using namespace std;
 
+pair<int, int> compute(vector<int> map)
+{
+    int count = 0;
+    for (int i = 0; i < map.size(); i++)
+    {
+        count += map[i];
+
+        if (count > 256)
+        {
+            int month = i + 1;
+            int days = 256 - (count - map[i]);
+
+            return make_pair(month, days);
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    return {0, 0};
+}
+
+bool isLeapYear(int year)
+{
+    if (year < 1919)
+    {
+        if (year % 4 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
 string dayOfProgrammer(int year)
 {
     string result;
     vector<int> non_leap_map = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     vector<int> leap_map = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    vector<int> julian = {31, 16, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    vector<int> julian = {31, 15, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     int monthResult = 0;
     int daysResult = 0;
@@ -18,105 +67,22 @@ string dayOfProgrammer(int year)
 
     if (year == 1918) // transition
     {
-        int count = 0;
-        for (int i = 0; i < julian.size(); i++)
-        {
-            count += julian[i];
-
-            if (count < 256)
-                continue;
-
-            if (count > 256)
-            {
-                monthResult = i + 1;
-                daysResult = 256 - (count - julian[i]) + 1;
-
-                break;
-            }
-        }
+        auto resultPair = compute(julian);
+        monthResult = resultPair.first;
+        daysResult = resultPair.second;
     }
 
-    else if (year < 1919) // julian
+    else if (isLeapYear(year))
     {
-        if (year % 4 == 0)
-        {
-            int count = 0;
-            for (int i = 0; i < leap_map.size(); i++)
-            {
-                count += leap_map[i];
-
-                if (count < 256)
-                    continue;
-
-                if (count > 256)
-                {
-                    monthResult = i + 1;
-                    daysResult = 256 - (count - leap_map[i]);
-
-                    break;
-                }
-            }
-        }
-        else
-        {
-            int count = 0;
-            for (int i = 0; i < non_leap_map.size(); i++)
-            {
-                count += non_leap_map[i];
-
-                if (count < 256)
-                    continue;
-
-                if (count > 256)
-                {
-                    monthResult = i + 1;
-                    daysResult = 256 - (count - non_leap_map[i]);
-
-                    break;
-                }
-            }
-        }
+        auto resultPair = compute(leap_map);
+        monthResult = resultPair.first;
+        daysResult = resultPair.second;
     }
-    else // gregorian
+    else
     {
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-        {
-            int count = 0;
-            for (int i = 0; i < leap_map.size(); i++)
-            {
-                count += leap_map[i];
-
-                if (count < 256)
-                    continue;
-
-                if (count > 256)
-                {
-                    monthResult = i + 1;
-                    daysResult = 256 - (count - leap_map[i]);
-
-                    break;
-                }
-            }
-        }
-        else
-        {
-            int count = 0;
-            for (int i = 0; i < non_leap_map.size(); i++)
-            {
-                count += non_leap_map[i];
-
-                if (count < 256)
-                    continue;
-
-                if (count > 256)
-                {
-                    monthResult = i + 1;
-                    daysResult = 256 - (count - non_leap_map[i]);
-
-                    break;
-                }
-            }
-        }
+        auto resultPair = compute(non_leap_map);
+        monthResult = resultPair.first;
+        daysResult = resultPair.second;
     }
 
     stringstream monthfmt;
