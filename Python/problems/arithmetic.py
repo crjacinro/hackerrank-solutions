c13bda08d7f7d7a334228222806ef949be7a1bd0
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2021-08-15 17:49:22 +0800

    Add python arithmetic and conditions

---
+++ Python/problems/arithmetic.py
@@ -0,0 +1,12 @@
+def arithmetic():
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print(a - b)
+    print(a * b)
+
+
+def division():
+    a = int(input())
+    b = int(input())
+    print("{0}\n{1}".format(a // b, a / b))
