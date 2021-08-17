70c2b10c5d8457dfc4a480017c6f1a548455822d
Serge Nino Martin Villasica <villasica.serge@gmail.com>
2021-08-17 23:01:57 +0800

    Add intro solutions

--- Python/problems/arithmetic.py
+++ Python/problems/arithmetic.py
@@ -10,3 +10,9 @@ def division():
     a = int(input())
     b = int(input())
     print("{0}\n{1}".format(a // b, a / b))
+
+
+def squares():
+    n = int(input())
+    for x in range(n):
+        print(x * x)
